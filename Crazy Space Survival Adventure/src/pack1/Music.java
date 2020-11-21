b4c23df561cf2a24c38107a31ebc3dcc4e973dae
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2020-11-21 17:42:27 +0100

    small adjustment 2

--- Crazy Space Survival Adventure/src/pack1/Music.java
+++ Crazy Space Survival Adventure/src/pack1/Music.java
@@ -27,7 +27,6 @@ public class Music {
 						clip.loop(Clip.LOOP_CONTINUOUSLY);
 						FloatControl volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
 						volume.setValue(-10);
-
 						Thread.sleep(clip.getMicrosecondLength() / 1000);
 						clip.stop(); 
 						clip.close();
