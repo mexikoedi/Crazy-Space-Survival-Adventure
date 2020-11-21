313c5b6b2e3d6bc0b7c6a61a0eb4a4f1d5e0eca3
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2020-11-21 17:41:40 +0100

    small adjustment

--- Crazy Space Survival Adventure/src/pack1/Music.java
+++ Crazy Space Survival Adventure/src/pack1/Music.java
@@ -27,10 +27,11 @@ public class Music {
 						clip.loop(Clip.LOOP_CONTINUOUSLY);
 						FloatControl volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
 						volume.setValue(-10);
-						
+
 						Thread.sleep(clip.getMicrosecondLength() / 1000);
 						clip.stop(); 
 						clip.close();
+
 					} catch (Exception e) {
 						e.printStackTrace();
 						System.out.println("Audio konnte nicht geladen werden");
